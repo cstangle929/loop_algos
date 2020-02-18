e0d3b5630fcb3fbf1913f70405ffa4604949f7d2
Chris Stangle <chris.stangle@gmail.com>
2020-02-18 13:31:58 -0600

    finished

--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -1,5 +1,50 @@
 // Take your previous solutions and write them using the other kind of loop.  
-// I.E.  If you wrote both of your algorithms using for loops, now write them out with while loops
+// I.E.  If you wrote both of your algorithms using for loops, 
+// now write them out with while loops
 
 
 // YOUR CODE HERE
+
+//3_fizzbuzz_while
+/*
+var num = 1;
+while (num <=30){   
+    if(num % 3 == 0 && num % 5 == 0){
+        console.log("FizzBuzz");
+    }
+    else if(num % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(num % 5 === 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(num);
+    }
+    num ++;
+}
+*/
+
+/*
+//2_sum_while
+
+var sum = 0
+var num = 1
+while(num <=10){
+    sum = num + sum
+    console.log("Num:" + num);
+    num++
+    console.log("Sum:" + sum);
+
+}
+*/
+
+
+//1_print_odds_while
+var num = 0
+while(num <= 20){  
+    if(num % 2 != 0){
+    console.log(num);
+    }
+num++
+}
